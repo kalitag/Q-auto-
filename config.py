bc064ee25b39fa033660031548fc1f15623f5f6f
kalitag <aaryankumargupta01@gmail.com>
2025-08-15 02:18:10 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,33 @@
+# Bot Configuration
+BOT_TOKEN = "8414049375:AAFMPUvB2u5KffNPsaAi3xu_DOiy-7dhHIg"
+BOT_USERNAME = "@Final_output_formatbot"
+
+# Supported Platforms
+SUPPORTED_PLATFORMS = [
+    "amazon.in", "flipkart.com", "meesho.com", 
+    "myntra.com", "ajio.com", "snapdeal.com", 
+    "wishlink.com"
+]
+
+# Supported Shorteners
+SUPPORTED_SHORTENERS = [
+    "cutt.ly", "fkrt.cc", "amzn-to.co", 
+    "bitli.in", "spoo.me", "wishlink.com", 
+    "da.gd"
+]
+
+# Default Values
+DEFAULT_PIN = "110001"
+DEFAULT_TIMEOUT = 5
+MAX_CONCURRENT_TASKS = 10
+
+# Gender Keywords
+GENDER_KEYWORDS = {
+    "women": ["women", "woman", "female", "ladies"],
+    "men": ["men", "man", "male", "gents"],
+    "kids": ["kids", "children", "baby", "infant"],
+    "unisex": ["unisex", "both"]
+}
+
+# Size Labels
+SIZE_LABELS = ["S", "M", "L", "XL", "XXL", "XXXL", "Free Size", "Free", "All"]
